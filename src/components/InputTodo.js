2811761a92d9e5336b63edea9df4925bd25a218a
Ghilain <ishimweghilain5050@gmail.com>
2022-06-07 22:59:39 +0200

    Add css style

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,40 +1,43 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 
 class InputTodo extends Component {
     state = {
-        title: ""
-      };
-      onChange = e => {
+      title: '',
+    };
+
+      onChange = (e) => {
         this.setState({
-            // title: e.target.value
-            [e.target.name]: e.target.value
-          });
+          // title: e.target.value
+          [e.target.name]: e.target.value,
+        });
       };
 
-      handleSubmit = e => {
+      handleSubmit = (e) => {
         e.preventDefault();
         if (this.state.title.trim()) {
-            this.props.addTodoProps(this.state.title)
-            this.setState({
-              title: "",
-            })
-          } else {
-            alert("Please write item")
-          }
+          this.props.addTodoProps(this.state.title);
+          this.setState({
+            title: '',
+          });
+        } else {
+          alert('Please write item');
+        }
       };
 
       render() {
-          return (
-          <form onSubmit={this.handleSubmit}>
-              <input 
-              type="text" 
-              placeholder="Add Todo..." 
-              value={this.state.title} 
+        return (
+          <form onSubmit={this.handleSubmit} className="form-container">
+            <input
+              type="text"
+              className="input-text"
+              placeholder="Add Todo..."
+              value={this.state.title}
               name="title"
-              onChange={this.onChange} />
-              <button>Submit</button>
-            </form>
-          )
-        }
+              onChange={this.onChange}
+            />
+            <button className="input-submit">Submit</button>
+          </form>
+        );
+      }
 }
-export default InputTodo
+export default InputTodo;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom"
-//component file
-import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(  
-<React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+// component file
+import TodoContainer from './components/TodoContainer';
+// style
+import './App.css';
+
+ReactDOM.render(
+  <React.StrictMode>
     <TodoContainer />
-</React.StrictMode>, document.getElementById("root"))
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
