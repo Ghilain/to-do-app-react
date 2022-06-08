d90b7d9b767c51881776f12c8649979a8b7d2fc1
Ghilain <ishimweghilain5050@gmail.com>
2022-06-08 06:39:20 +0200

    Refactor using function componet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // component file
-import TodoContainer from './components/TodoContainer';
+import TodoContainer from "./functionBased/components/TodoContainer"
 // style
-import './App.css';
+import "./functionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
